349b9a96b6b424e1047b7b539c3f74a83b10938c
“Sanni <sanniyouboty@gmail.com>
2018-10-29 19:53:18 -0700

    Toggling visibility on load

--- src/Gif.js
+++ src/Gif.js
@@ -2,14 +2,27 @@ import App from "./App";
 import React, { Component } from "react";
 
 class Gif extends Component {
+  // when our vid has loaded, we add loaded class name
+  //otherwise the vid stays hidden
+  constructor(props) {
+    super(props);
+    this.state = {
+      loaded: false
+    };
+  }
+
   render() {
+    const loaded = this.state;
     const { images } = this.props;
     return (
       <video
-        className="grid-item video"
+        //when we have the loaded state as true, we add a loaded class
+        className={`grid-item video ${loaded && "loaded"}`}
         autoPlay
         loop
         src={images.original.mp4}
+        // when the vid leads, we set the loaded state to be true
+        onLoadedData={() => this.setState({ loaded: true })}
       />
     );
   }
