76e1b05b057fcac818010dc49c22f0e7ee6829bb
Rik Lomas <rik@superhi.com>
2018-11-09 11:18:04 -0500

    building a production ready folder

---
+++ precache-manifest.9a577f62b53030e09b292aaf4aaaffe7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "be00fc4a29d03016e78b28c9943e3f51",
+    "url": "/static/media/loader.be00fc4a.svg"
+  },
+  {
+    "revision": "5b9a3ae1255e28dc936234fe3c03e06c",
+    "url": "/static/media/close-icon.5b9a3ae1.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d94ab13614ca3b22d35a",
+    "url": "/static/js/main.d94ab136.chunk.js"
+  },
+  {
+    "revision": "834405f2e9effa094619",
+    "url": "/static/js/1.834405f2.chunk.js"
+  },
+  {
+    "revision": "d94ab13614ca3b22d35a",
+    "url": "/static/css/main.d64d86b0.chunk.css"
+  },
+  {
+    "revision": "64803d33b359b8e0821624c7439ae941",
+    "url": "/index.html"
+  }
+];
