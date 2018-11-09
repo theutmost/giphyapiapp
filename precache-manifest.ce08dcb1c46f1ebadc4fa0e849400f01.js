41479010fb0e30692d20ee14ae55fdf06c0c1d2f
Rik Lomas <rik@superhi.com>
2018-11-09 11:23:00 -0500

    Updates

---
+++ precache-manifest.ce08dcb1c46f1ebadc4fa0e849400f01.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "be00fc4a29d03016e78b28c9943e3f51",
+    "url": "/giphyapiapp/static/media/loader.be00fc4a.svg"
+  },
+  {
+    "revision": "5b9a3ae1255e28dc936234fe3c03e06c",
+    "url": "/giphyapiapp/static/media/close-icon.5b9a3ae1.svg"
+  },
+  {
+    "revision": "57cf108170977d4d57bc",
+    "url": "/giphyapiapp/static/js/runtime~main.57cf1081.js"
+  },
+  {
+    "revision": "b216b95cda26b6393e19",
+    "url": "/giphyapiapp/static/js/main.b216b95c.chunk.js"
+  },
+  {
+    "revision": "834405f2e9effa094619",
+    "url": "/giphyapiapp/static/js/1.834405f2.chunk.js"
+  },
+  {
+    "revision": "b216b95cda26b6393e19",
+    "url": "/giphyapiapp/static/css/main.d64d86b0.chunk.css"
+  },
+  {
+    "revision": "12c526e18516094cd38d1cf0639ff067",
+    "url": "/giphyapiapp/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9a577f62b53030e09b292aaf4aaaffe7.js"
+  "/giphyapiapp/precache-manifest.ce08dcb1c46f1ebadc4fa0e849400f01.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/giphyapiapp/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
